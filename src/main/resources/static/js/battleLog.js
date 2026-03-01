ac873653611adc5e877f6a14e66db7715e7f8c5b
FFF <191376912+fumiya-0238@users.noreply.github.com>
2026-03-01 22:56:20 +0900

    Delete src/main/resources directory

--- src/main/resources/static/js/battleLog.js
+++
@@ -1,9 +0,0 @@
-function showBattleLog(data){
-	logList.innerHTML = '';
-	
-	for (let i = 0, l = parseInt(data.length); i < l; i++) {
-		const li = document.createElement('li');
-		li.innerHTML=data[i];
-		logList.appendChild(li);
-		}
-	}
--- src/main/resources/static/js/debug.js
+++
@@ -1,4 +0,0 @@
-const debug = document.getElementById("debug");
-debug.style.top = parseInt(titleLocation.top) + 'px';
-debug.style.left = parseInt(titleLocation.left) + 'px';
-const debugDungeons = document.getElementById("debugDungeons");
